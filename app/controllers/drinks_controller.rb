36c7f8514a75ff926dbec8e939b22c48ebb0fa5f
toastcorners <wise.melissa19@gmail.com>
2020-05-05 12:32:02 -0400

    drink and user index and show

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,4 +1,5 @@
 class DrinksController < ApplicationController
+    before_action :find_drink, only: [:show, :edit]
 
     def index
     @drinks = Drink.all
@@ -12,5 +13,25 @@ class DrinksController < ApplicationController
     @drink = Drink.new
     end
 
+    def create 
+    @drink = Drink.new(drink_params)
+    if @drink.save
+        redirect_to drink_path(@drink)
+    else
+        flash[:errors] = @drink.errors.full_messages
+        redirect_to new_drink_path
+        end
+    end
 
+
+
+    private
+
+    def drink_params
+        params.require(:drink).permit(:name, :recipe, :likes => 0)
+    end
+
+    def find_drink
+        @drink = Drink.find(params[:id])
+    end
 end
