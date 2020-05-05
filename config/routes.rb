1bfe61c57a7cb353628df250168dfb381b2eebe3
toastcorners <wise.melissa19@gmail.com>
2020-05-05 09:35:55 -0400

    routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :drinks
+  resources :users
+  resources :products
+  resources :user_drinks
+  resources :product_drinks
+  resources :user_products
 end
